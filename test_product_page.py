0c956342a8eec5d5f7bf135b262611b9cbedf9db
Mike <zzzz@ttt.ru>
2019-07-14 15:10:35 +0300

    modify - test_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -30,14 +30,10 @@ def test_guest_can_add_product_to_cart(browser):
     page.check_item_name()
     page.check_item_price()
 
-
-
 def test_guest_cant_see_success_message(browser):
     page = ProductPage(browser, LINK)
     page.open()
     page.should_not_be_success_message()
-
-
 
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -72,7 +68,6 @@ def test_guest_add_item_to_cart(browser):
     cartpage = CartPage(browser,browser.current_url)
     cartpage.should_be_cart_have_text()
     cartpage.should_be_cart_is_emtpy()
-
 
 class TestUserAddToCartFromProductPage(object):
     @pytest.fixture(scope="function", autouse=True)
